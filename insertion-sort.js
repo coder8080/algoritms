a56a7f61471a100d2df9be1755f0d0db2345bcef
Roman Kolerov <coder8080@mail.ru>
2022-06-26 10:05:00 +0300

    Insertion sort

---
+++ insertion-sort.js
@@ -0,0 +1,23 @@
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    const current = arr[i]
+    let j = i - 1
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j]
+      j--
+    }
+    arr[j + 1] = current
+  }
+}
+
+const insertionSortInverted = (arr) => {
+  for (let i = arr.length - 2; i >= 0; i--) {
+    const current = arr[i]
+    let j = i + 1
+    while (j < arr.length && arr[j] < current) {
+      arr[j - 1] = arr[j]
+      j++
+    }
+    arr[j - 1] = current
+  }
+}
